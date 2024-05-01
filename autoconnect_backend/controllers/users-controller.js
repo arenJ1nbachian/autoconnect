804d1daf7602110192645d2a2a854c8e2ee18dbf
ArenJinbachian <2036555@cmontmorency.qc.ca>
2024-04-30 23:04:41 -0400

    Corrected CSS position of page elements, fixed variable names, added registration handling in the backend with password hashing, implemented cross-origin request authorization, and added a missing field to the user schema

--- autoconnect_backend/controllers/users-controller.js
+++ autoconnect_backend/controllers/users-controller.js
@@ -1,6 +1,35 @@
+const user = require("../models/user");
 const Users = require("../models/user");
+const bcrypt = require("bcrypt");
 
-const createUser = async (req, res, next) => {};
+const createUser = async (req, res, next) => {
+  const { firstName, lastName, email, phoneNumber, userName, password } =
+    req.body;
+
+  try {
+    let existingUser = await Users.findOne({ email: email });
+    if (existingUser) {
+      return res.status(409).json({ message: "User already exists" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new Users({
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      phoneNumber: phoneNumber,
+      userName: userName,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+
+    res.status(201).json({ message: "User created successfully." });
+  } catch (error) {
+    console.error("Error creating user:", error);
+    return res.status(500).json({ message: "Failed to create user." });
+  }
+};
 
 const login = async (req, res, next) => {};
 
--- autoconnect_backend/models/user.js
+++ autoconnect_backend/models/user.js
@@ -3,11 +3,12 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
+  email: { type: String, required: true },
   phoneNumber: { type: String, required: false },
   userName: { type: String, required: true },
   password: { type: String, required: true },
   favorites: [
-    { type: mongoose.Types.ObjectId, required: true, ref: "Listings" },
+    { type: mongoose.Types.ObjectId, required: false, ref: "Listings" },
   ],
 });
 
