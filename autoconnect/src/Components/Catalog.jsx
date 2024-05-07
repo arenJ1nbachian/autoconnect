e5c556fe7c6a232be714e374353c556f9cadcfec
ArenJinbachian <2036555@cmontmorency.qc.ca>
2024-05-07 13:06:12 -0400

    Implemented dynamic filtering for ad display

--- autoconnect/src/Components/Catalog.jsx
+++ autoconnect/src/Components/Catalog.jsx
@@ -1,6 +1,12 @@
-import { useEffect } from "react";
+import { Box, Grid, Paper } from "@mui/material";
+import { useEffect, useState } from "react";
+import carIcon from "../img/default_car_image.png";
+import { useNavigate } from "react-router-dom";
 
 const Catalog = ({ selectedFilters }) => {
+  const [result, setResult] = useState(undefined);
+  const navigate = useNavigate();
+
   useEffect(() => {
     const {
       makes,
@@ -43,6 +49,7 @@ const Catalog = ({ selectedFilters }) => {
         if (!response.ok) throw new Error("Failed to fetch filter data");
 
         const data = await response.json();
+        setResult(data);
         console.log(data);
       } catch (err) {
         console.error(err);
@@ -51,6 +58,117 @@ const Catalog = ({ selectedFilters }) => {
 
     fetchQuery();
   }, [selectedFilters]);
+
+  const handleClick = (id) => {
+    navigate(`/listings/details/${id}`);
+  };
+
+  return (
+    <>
+      {result &&
+        result.queryResult.map((listing) => (
+          <Grid item xs={3}>
+            <Box
+              onClick={() => handleClick(listing._id)}
+              sx={{
+                cursor: "pointer",
+                marginTop: "5vh",
+                position: "relative",
+                borderRadius: "15px",
+                border: "2px solid #0066cc",
+                width: "270.5px",
+                aspectRatio: "343/290",
+                backgroundColor: "White",
+                backgroundSize: "cover",
+                padding: "20px",
+              }}
+            >
+              <div>
+                {result && (
+                  <Box
+                    sx={{
+                      position: "absolute",
+                      top: 0,
+                      left: 0,
+                      width: "100%",
+                      height: "100%",
+                      flexDirection: "column",
+                      display: "flex",
+
+                      color: "white",
+                      fontSize: "24px",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    <img
+                      style={{
+                        width: "100%",
+                        height: "100%",
+                        objectFit: "cover",
+                        borderRadius: "15px",
+                      }}
+                      src={listing.images.length ? listing.images[0] : carIcon}
+                      alt="main"
+                    />
+                    <div
+                      style={{
+                        marginTop: "1vh",
+                        marginLeft: "1vw",
+                        fontSize: 13,
+                        display: "flex",
+                        flexDirection: "column",
+                        color: "black",
+                      }}
+                    >
+                      <div
+                        style={{
+                          display: "flex",
+                        }}
+                      >
+                        <div>
+                          {`${listing.year} ${listing.make}
+                      ${listing.model}`}
+                        </div>
+                        <div
+                          style={{
+                            marginLeft: "auto",
+                            marginRight: "1.5vw",
+                            border: "2px solid #0066cc",
+                            borderRadius: "8px",
+                          }}
+                        >
+                          {new Intl.NumberFormat("en-US").format(
+                            parseInt(listing.price)
+                          ) + " $"}
+                        </div>
+                      </div>
+                      <div style={{ height: "21px" }}>
+                        {new Intl.NumberFormat("en-US").format(
+                          parseInt(listing.km)
+                        ) + " km"}
+                      </div>
+                      <div style={{ height: "21px" }}>
+                        {listing.transmission}{" "}
+                      </div>
+                      <div
+                        style={{
+                          fontSize: "10px",
+                          color: "grey",
+                          marginLeft: "auto",
+                          marginRight: "1vw",
+                        }}
+                      >
+                        {listing._id}
+                      </div>
+                    </div>
+                  </Box>
+                )}
+              </div>
+            </Box>
+          </Grid>
+        ))}
+    </>
+  );
 };
 
 export default Catalog;
--- autoconnect/src/Layouts/Listings.jsx
+++ autoconnect/src/Layouts/Listings.jsx
@@ -9,6 +9,7 @@ import {
   FormControlLabel,
   Typography,
   Slider,
+  Grid,
 } from "@mui/material";
 import { useEffect, useState } from "react";
 import carLogo from "../Data/carLogo.json";
@@ -129,199 +130,213 @@ const Listings = () => {
       >
         Voiture d'occasion
       </Typography>
-      <Card
-        sx={{
-          marginTop: "3vh",
-          marginLeft: "2vw",
-          width: 350,
-          height: 650,
-          overflowY: "auto",
-          borderRadius: 7,
-        }}
-      >
-        <Accordion>
-          <AccordionSummary
-            expandIcon={<ExpandMore />}
-            aria-controls="panel1-content"
-            id="panel1-header"
+      <Grid container direction="row" justifyContent="center" spacing={2}>
+        <Grid item xs={3}>
+          <Card
             sx={{
-              minHeight: "72.2px",
-              "& .MuiAccordionSummary-content": {
-                margin: "12px 0",
-              },
+              marginLeft: "4vw",
+              marginTop: "3vh",
+              width: 350,
+              height: 650,
+              overflowY: "auto",
+              borderRadius: 7,
             }}
           >
-            Marque et Modèle
-          </AccordionSummary>
-          <AccordionDetails>
-            {renderRadioGroup("makes", filterData.makes, true)}
-          </AccordionDetails>
-        </Accordion>
-        <Accordion>
-          <AccordionSummary
-            expandIcon={<ExpandMore />}
-            aria-controls="panel2-content"
-            id="panel2-header"
-            sx={{
-              minHeight: "72.2px",
-              "& .MuiAccordionSummary-content": {
-                margin: "12px 0",
-              },
-            }}
-          >
-            Carrosserie
-          </AccordionSummary>
-          <AccordionDetails>
-            {renderRadioGroup("bodies", filterData.bodies)}
-          </AccordionDetails>
-        </Accordion>
-        <Accordion>
-          <AccordionSummary
-            expandIcon={<ExpandMore />}
-            aria-controls="panel3-content"
-            id="panel3-header"
-            sx={{
-              minHeight: "72.2px",
-              "& .MuiAccordionSummary-content": {
-                margin: "12px 0",
-              },
-            }}
-          >
-            Prix
-          </AccordionSummary>
-          <AccordionDetails>
-            {renderSlider(
-              "priceRange",
-              filterData.priceRange[0],
-              filterData.priceRange[1],
-              1000,
-              "Price"
-            )}
-          </AccordionDetails>
-        </Accordion>
-        <Accordion>
-          <AccordionSummary
-            expandIcon={<ExpandMore />}
-            aria-controls="panel4-content"
-            id="panel4-header"
-            sx={{
-              minHeight: "72.2px",
-              "& .MuiAccordionSummary-content": {
-                margin: "12px 0",
-              },
-            }}
-          >
-            Km
-          </AccordionSummary>
-          <AccordionDetails>
-            {renderSlider(
-              "kmRange",
-              filterData.kmRange[0],
-              filterData.kmRange[1],
-              1000,
-              "Km"
-            )}
-          </AccordionDetails>
-        </Accordion>
-        <Accordion>
-          <AccordionSummary
-            expandIcon={<ExpandMore />}
-            aria-controls="panel5-content"
-            id="panel5-header"
-            sx={{
-              minHeight: "72.2px",
-              "& .MuiAccordionSummary-content": {
-                margin: "12px 0",
-              },
-            }}
-          >
-            Année
-          </AccordionSummary>
-          <AccordionDetails>
-            {renderSlider(
-              "yearRange",
-              filterData.yearRange[0],
-              filterData.yearRange[1],
-              1,
-              "Year"
-            )}
-          </AccordionDetails>
-        </Accordion>
-        <Accordion>
-          <AccordionSummary
-            expandIcon={<ExpandMore />}
-            aria-controls="panel6-content"
-            id="panel6-header"
-            sx={{
-              minHeight: "72.2px",
-              "& .MuiAccordionSummary-content": {
-                margin: "12px 0",
-              },
-            }}
-          >
-            Transmission
-          </AccordionSummary>
-          <AccordionDetails>
-            {renderRadioGroup("transmissions", filterData.transmissions)}
-          </AccordionDetails>
-        </Accordion>
-        <Accordion>
-          <AccordionSummary
-            expandIcon={<ExpandMore />}
-            aria-controls="panel7-content"
-            id="panel7-header"
-            sx={{
-              minHeight: "72.2px",
-              "& .MuiAccordionSummary-content": {
-                margin: "12px 0",
-              },
-            }}
-          >
-            Traction
-          </AccordionSummary>
-          <AccordionDetails>
-            {renderRadioGroup("tractions", filterData.tractions)}
-          </AccordionDetails>
-        </Accordion>
-        <Accordion>
-          <AccordionSummary
-            expandIcon={<ExpandMore />}
-            aria-controls="panel8-content"
-            id="panel8-header"
-            sx={{
-              minHeight: "72.2px",
-              "& .MuiAccordionSummary-content": {
-                margin: "12px 0",
-              },
-            }}
-          >
-            Carburant
-          </AccordionSummary>
-          <AccordionDetails>
-            {renderRadioGroup("fuels", filterData.fuels)}
-          </AccordionDetails>
-        </Accordion>
-        <Accordion>
-          <AccordionSummary
-            expandIcon={<ExpandMore />}
-            aria-controls="panel9-content"
-            id="panel9-header"
-            sx={{
-              minHeight: "72.2px",
-              "& .MuiAccordionSummary-content": {
-                margin: "12px 0",
-              },
-            }}
-          >
-            Couleur
-          </AccordionSummary>
-          <AccordionDetails>
-            {renderRadioGroup("colors", filterData.colors)}
-          </AccordionDetails>
-        </Accordion>
-      </Card>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMore />}
+                aria-controls="panel1-content"
+                id="panel1-header"
+                sx={{
+                  minHeight: "72.2px",
+                  "& .MuiAccordionSummary-content": {
+                    margin: "12px 0",
+                  },
+                }}
+              >
+                Marque et Modèle
+              </AccordionSummary>
+              <AccordionDetails>
+                {renderRadioGroup("makes", filterData.makes, true)}
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMore />}
+                aria-controls="panel2-content"
+                id="panel2-header"
+                sx={{
+                  minHeight: "72.2px",
+                  "& .MuiAccordionSummary-content": {
+                    margin: "12px 0",
+                  },
+                }}
+              >
+                Carrosserie
+              </AccordionSummary>
+              <AccordionDetails>
+                {renderRadioGroup("bodies", filterData.bodies)}
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMore />}
+                aria-controls="panel3-content"
+                id="panel3-header"
+                sx={{
+                  minHeight: "72.2px",
+                  "& .MuiAccordionSummary-content": {
+                    margin: "12px 0",
+                  },
+                }}
+              >
+                Prix
+              </AccordionSummary>
+              <AccordionDetails>
+                {renderSlider(
+                  "priceRange",
+                  filterData.priceRange[0],
+                  filterData.priceRange[1],
+                  1000,
+                  "Price"
+                )}
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMore />}
+                aria-controls="panel4-content"
+                id="panel4-header"
+                sx={{
+                  minHeight: "72.2px",
+                  "& .MuiAccordionSummary-content": {
+                    margin: "12px 0",
+                  },
+                }}
+              >
+                Km
+              </AccordionSummary>
+              <AccordionDetails>
+                {renderSlider(
+                  "kmRange",
+                  filterData.kmRange[0],
+                  filterData.kmRange[1],
+                  1000,
+                  "Km"
+                )}
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMore />}
+                aria-controls="panel5-content"
+                id="panel5-header"
+                sx={{
+                  minHeight: "72.2px",
+                  "& .MuiAccordionSummary-content": {
+                    margin: "12px 0",
+                  },
+                }}
+              >
+                Année
+              </AccordionSummary>
+              <AccordionDetails>
+                {renderSlider(
+                  "yearRange",
+                  filterData.yearRange[0],
+                  filterData.yearRange[1],
+                  1,
+                  "Year"
+                )}
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMore />}
+                aria-controls="panel6-content"
+                id="panel6-header"
+                sx={{
+                  minHeight: "72.2px",
+                  "& .MuiAccordionSummary-content": {
+                    margin: "12px 0",
+                  },
+                }}
+              >
+                Transmission
+              </AccordionSummary>
+              <AccordionDetails>
+                {renderRadioGroup("transmissions", filterData.transmissions)}
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMore />}
+                aria-controls="panel7-content"
+                id="panel7-header"
+                sx={{
+                  minHeight: "72.2px",
+                  "& .MuiAccordionSummary-content": {
+                    margin: "12px 0",
+                  },
+                }}
+              >
+                Traction
+              </AccordionSummary>
+              <AccordionDetails>
+                {renderRadioGroup("tractions", filterData.tractions)}
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMore />}
+                aria-controls="panel8-content"
+                id="panel8-header"
+                sx={{
+                  minHeight: "72.2px",
+                  "& .MuiAccordionSummary-content": {
+                    margin: "12px 0",
+                  },
+                }}
+              >
+                Carburant
+              </AccordionSummary>
+              <AccordionDetails>
+                {renderRadioGroup("fuels", filterData.fuels)}
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMore />}
+                aria-controls="panel9-content"
+                id="panel9-header"
+                sx={{
+                  minHeight: "72.2px",
+                  "& .MuiAccordionSummary-content": {
+                    margin: "12px 0",
+                  },
+                }}
+              >
+                Couleur
+              </AccordionSummary>
+              <AccordionDetails>
+                {renderRadioGroup("colors", filterData.colors)}
+              </AccordionDetails>
+            </Accordion>
+          </Card>
+        </Grid>
+
+        <Grid
+          sx={{ overflow: "auto", width: "auto", height: "75vh" }}
+          item
+          xs={9}
+        >
+          <Grid container>
+            <Catalog selectedFilters={selectedFilters} />
+          </Grid>
+        </Grid>
+      </Grid>
+
       {console.log("What is selected", selectedFilters)}
-      <Catalog selectedFilters={selectedFilters} />
     </>
   );
 };
